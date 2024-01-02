5001187cfb8625f6506f70bde2fadc4fb8b968d3
Shimada666 <649940882@qq.com>
2024-01-02 11:19:29 +0800

    feat: 增加 eslint

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,54 +1,37 @@
 import { defineConfig } from 'vitepress'
+import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar'
 
 const nav = [
   {
     text: '文档',
     activeMatch: '/docs/',
-    link: '/docs/module1'
-  }
+    link: '/docs/module1',
+  },
 ]
-
-const sidebar = {
-  '/docs/': [
-    {
-      text: '模块1',
-      link: '/docs/module1',
-      items: [
-        { text: 'module1', link: '/docs/module1' },
-        { text: 'xxx', link: '/docs/xx' }
-      ]
-    },
-    {
-      text: '模块2',
-      link: '/docs/module2',
-      items: [
-        { text: 'module2', link: '/docs/module2' }
-      ]
-    },
-  ]
-}
-
 
 export default defineConfig({
   title: 'docs-template',
   description: '一个 vitepress 文档模板，适用于 github 代码文档',
   lastUpdated: true,
+  vite: {
+    // @ts-expect-error autobar
+    plugins: [AutoSidebar({ prefix: '.', path: '.', collapsed: true })],
+  },
   themeConfig: {
     socialLinks: [
       {
         icon: 'github',
-        link: 'https://github.com/Shimada666/docs-template'
-      }
+        link: 'https://github.com/Shimada666/docs-template',
+      },
     ],
     editLink: {
       pattern: 'https://github.com/shimada666/docs-template/edit/master/:path',
-      text: 'Edit this page on GitHub'
+      text: 'Edit this page on GitHub',
     },
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present Shimada666'
+      copyright: 'Copyright © 2022-present Shimada666',
     },
     nav,
-    sidebar
-  }
+  },
 })
